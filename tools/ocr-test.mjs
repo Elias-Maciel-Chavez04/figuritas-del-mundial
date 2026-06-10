049ec49ff2ac85e72c280a6f0c7e2ffe1b692ca0
Elias Maciel <chaveznicolas774@gmail.com>
2026-06-10 08:21:36 -0300

    elimina todos los archivos

--- tools/ocr-test.mjs
+++
@@ -1,7 +0,0 @@
-import { createWorker } from 'tesseract.js'
-
-const file = process.argv[2]
-const worker = await createWorker('eng')
-const { data } = await worker.recognize(file)
-console.log(data.text)
-await worker.terminate()
